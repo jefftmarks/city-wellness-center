37b0cf171849b3bae664ebe82486f5acf0c9a5e5
Jeff Marks <jeff.t.marks@gmail.com>
2022-10-07 09:51:31 -0400

    finished doctor about page

--- client/src/components/home/DoctorCard.js
+++ client/src/components/home/DoctorCard.js
@@ -7,9 +7,9 @@ function DoctorCard({ doctor }) {
 			<img src={doctor.image} alt="doctor" />
 			<div className="doctor-info">
 				<p>{doctor.first_name} {doctor.last_name}</p>
-				<p>{doctor.certification}</p>
+				<p>{doctor.certification ? doctor.certification : ""}</p>
 				<div className="card-line"></div>
-				<p>"{doctor.bio}"</p>
+				<p>{doctor.bio ? `"${doctor.bio}"` : ""}</p>
 			</div>
 		</div>
 	);
